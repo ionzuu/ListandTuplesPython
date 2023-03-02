c41396ce199aea4665cf0cb5b9166f8a04a9beb6
ionzuu <jonathan.costilla@protonmail.com>
2023-03-01 22:17:49 -0600

    Create ListandTuples.py

---
+++ ListandTuples.py
@@ -0,0 +1,11 @@
+#List and tuples
+
+Rating = ( 10 , 9 , 6 , 8 , 10 , 9 , 2 )
+
+tuple1 = ( 'tuple' , 12332 , 1.5)
+
+tuple2 = tuple1 + ( 'hard rock' , 1.5 , 23454325 , 'this is an example of sum of two numbers' );
+
+#tuples: slicing
+tuple2[0:3]
+tuple2[3:5]
